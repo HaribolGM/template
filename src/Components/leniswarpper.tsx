2677b5af1fb0ca03b35b3a80016241dc5c9f9473
Gaurav Mali <gauravmali846@gmail.com>
2024-01-26 15:41:18 +0530

    Add SmoothScrolling component to layout.tsx

---
+++ src/Components/leniswarpper.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+interface SmoothScrollingProps {
+  children: ReactNode;
+}
+
+function SmoothScrolling({ children }: SmoothScrollingProps) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
